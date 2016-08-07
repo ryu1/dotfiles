bd1455597fc96743860a3d4347756b77cf654249
石塚隆一 <jeremy.hillary.boob@gmail.com>
2016-08-07 16:19:38 +0900

    Add vim-airline with dein.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ ln -s ${DOTFILES_DIR}/.zshrc ~/.zshrc
 # vim
 ln -s ${DOTFILES_DIR}/.vimrc ~/.vimrc
 ln -s ${DOTFILES_DIR}/.vim ~/.vim
+ln -s ${DOTFILES_DIR}/.dein.toml ~/.dein.toml
 
 # git
 ln -s ${DOTFILES_DIR}/.gitconfig ~/.gitconfig
 ln -s ${DOTFILES_DIR}/.gitignore_global ~/.gitignore_global
+
